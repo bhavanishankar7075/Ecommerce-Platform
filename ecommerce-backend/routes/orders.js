dd694a772d8ce227af6d92b5941d766c4d34acc1
bhavanishankar7075 <bhavanimandala2004@gmail.com>
2025-06-26 17:47:37 +0530

    backend

--- ecommerce-backend/routes/orders.js
+++ ecommerce-backend/routes/orders.js
@@ -374,7 +374,7 @@ router.post('/', authMiddleware, async (req, res) => {
       payment_method_types: ['card'],
       line_items: lineItems,
       mode: 'payment',
-      success_url: `https://frontend-8uy4.onrender.com//success?session_id={CHECKOUT_SESSION_ID}`,
+      success_url: `https://frontend-8uy4.onrender.com/success?session_id={CHECKOUT_SESSION_ID}`,
       cancel_url: `https://frontend-8uy4.onrender.com/failure`,
       shipping_address_collection: {
         allowed_countries: ['US', 'IN'],
